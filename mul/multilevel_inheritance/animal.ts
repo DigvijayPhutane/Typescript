6aa1b385d3541565e0419b12c97b9c568862c692
Digvijay Phutane <digvijay.phutane@gmail.com>
2022-05-11 10:56:28 +0530

    Update animal.ts

--- mul/multilevel_inheritance/animal.ts
+++ mul/multilevel_inheritance/animal.ts
@@ -1,1 +1,20 @@
 
+class Animal{
+    cat():void{
+        console.log("Eating");
+    }
+}
+class Dog extends Animal{
+    bark():void{
+        console.log("Barking");
+    }
+}
+class Pup extends Dog{
+    weep():void{
+        console.log("weeping");
+    }
+}
+let obj1 = new Pup();
+obj1.cat();
+obj1.bark();
+obj1.weep();
